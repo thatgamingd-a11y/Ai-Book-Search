57cdadfe9c4201e448c0f3df8bcfbffb966fcc22
thatgamingd-a11y <thatgamingd@gmail.com>
2026-03-05 17:45:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-script
+function searchBook() {
+
+let book = document.getElementById("book").value
+let query = document.getElementById("query").value.toLowerCase()
+
+let paragraphs = book.split("\n")
+
+let results = paragraphs.filter(p => 
+    p.toLowerCase().includes(query)
+)
+
+let resultHTML = ""
+
+results.slice(0,5).forEach(r=>{
+    resultHTML += "<p>" + r + "</p><hr>"
+})
+
+document.getElementById("results").innerHTML = resultHTML
+
+}
